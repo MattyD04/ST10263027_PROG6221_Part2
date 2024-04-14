02f360ab94568a50d8d646d537ea4f47d4930028
MattyD04 <matthewdarlow2004@gmail.com>
2024-04-14 10:05:43 +0200

    Add project files.

---
+++ Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// utilised Claude AI for debugging and to mainly fix a debugging target error
+///
+///
+namespace RecipeManagerPOE
+{
+    internal class Recipe
+    {
+       
+        //****************************************************************************//
+    }
+}
+//****************************************end of file***********************************************//
+
