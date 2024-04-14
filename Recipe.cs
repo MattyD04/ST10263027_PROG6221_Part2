f112c1a3398e02b9db590978fb22c361f0bded23
MattyD04 <matthewdarlow2004@gmail.com>
2024-04-14 10:07:33 +0200

    created the variables for the recipe class

--- Recipe.cs
+++ Recipe.cs
@@ -12,7 +12,15 @@ namespace RecipeManagerPOE
 {
     internal class Recipe
     {
-       
+        public string ingredients { get; set; }
+        public string RecipeName { get; set; } //name of the recipe
+        public int NumIng { get; set; } //variable to store the amount of ingredients will be needed for the recipe
+        public string InName { get; set; } //the name of the ingredient
+        public double InQuant { get; set; } //variable to store quantity of ingredients used
+        public string unit { get; set; } //unit of measurement used for the ingredients
+        public List<string> StepsLists { get; set; } = new List<string>(); //this list will be used to store the steps for the recipe
+        public List<string> IngredientsList { get; set; } = new List<string>(); //array list to store the ingredients
+
         //****************************************************************************//
     }
 }
